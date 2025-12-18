42c746f53c898b30281aeff6309937f16fcdc3f3
Kate-8686 <ust.katerina@gmail.com>
2025-12-19 01:53:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/englishcoursefigma/', // ← раскомментировано + строчные буквы!
+  base: '/Englishcoursefigma/', // ← раскомментировано + строчные буквы!
 });
